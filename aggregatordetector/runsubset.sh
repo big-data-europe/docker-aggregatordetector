f5594f59006c37b100299082a5582a575ed6fee0
indiana <gioargyr@gmail.com>
2018-02-01 13:51:21 +0200

    Initial commit of aggregatordetector 3rd cycle.
    Architecture of docker-images in layers.

--- aggregatordetector/runsubset.sh
+++ aggregatordetector/runsubset.sh
@@ -1,16 +1,1 @@
 #!/bin/bash
-
-export APP=/javasparkapps/subset.jar
-export IMG_DIR=$1
-export IMG_NAME=$2
-export POLYGON=${@:3}
-
-printf "\n"
-echo "Submiting ${APP} with arguments:"
-echo "args[0] = ${IMG_DIR}"
-echo "args[1] = ${IMG_NAME}"
-echo "args[2] = ${POLYGON}"
-echo "Computing subset..."
-
-java -Xmx12g -jar ${APP} ${IMG_DIR} ${IMG_NAME} ${POLYGON}
-echo "...image ${IMG_NAME} has been subseted."
